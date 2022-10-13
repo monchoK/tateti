04479de98cfb47b1bc0259099655f952ab0b88ac
monchoK <kevinguevara9876mail.com>
2022-10-13 16:24:17 -0300

    elimine los datos de la base de datos del config.py

--- src/config.py
+++ src/config.py
@@ -1,10 +1,6 @@
 #Clase configuraciones de desarrollador, devuelve  un diccionario que tiene la instancia de las clases
 class DevelopmentConfig():
     DEBUG=True 
-    MYSQL_HOST ="127.0.0.1"
-    MYSQL_USER ="root"
-    MYSQL_PASSWORD ="secret"
-    MYSQL_DB="Tateti"
-
+   
 config = {"development":DevelopmentConfig}
 
