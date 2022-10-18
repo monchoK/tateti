9cc62783bd069204c6bb83ba882a11eeec551965
monchoK <kevinguevara9876mail.com>
2022-10-18 19:19:34 -0300

    mas cambios

--- src/validar.py
+++ src/validar.py
@@ -7,10 +7,10 @@ def validar_names(names: str) -> bool:
     names = names.strip()
     return (len(names) > 0 and len(names) <= 30)
 
-# Valida que los puntos estén entre 1 y 999.
+# Valida que los puntos estén entre 1 y 9999.
 def validar_points(points: str) -> bool:
     points_texto = str(points)
     if points_texto.isnumeric():
-        return (int(points) >= 1 and int(points) <= 999)
+        return (int(points) >= 1 and int(points) <= 9999)
     else:
         return False
